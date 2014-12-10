5fb34eb0dddfa9e39d39f070bfbb2ee00dce157c
Edson Yanaga <edson@yanaga.com.br>
2014-12-10 06:52:05 -0200

    Added some javadocs do the ListTransformer methods.

--- src/main/java/com/github/javacodekata/lambda/stream/ListTransformer.java
+++ src/main/java/com/github/javacodekata/lambda/stream/ListTransformer.java
@@ -20,14 +20,36 @@ public class ListTransformer {
 		return new ListTransformer(ImmutableList.copyOf(values));
 	}
 
+	/**
+	 * This method should take the String List and sort all the String elements in ascending (ASCII) order.
+	 * @return The sorted values in ascending ASCII order.
+	 */
 	public List<String> getSortedStrings() {
 		return values;
 	}
 
+	/**
+	 * This method should take the String List and:
+	 * <ol>
+	 *     <li>filter the elements that contains one or more digits;</li>
+	 *     <li>transform (map) the remaining Strings into Integers;</li>
+	 *     <li>sort the Integers in ascending order.</li>
+	 * </ol>
+	 * @return
+	 */
 	public List<Integer> getSortedIntegers() {
 		return new LinkedList<>();
 	}
 
+	/**
+	 * This method should take the String List and:
+	 * <ol>
+	 *     <li>filter the elements that contains one or more digits;</li>
+	 *     <li>transform (map) the remaining Strings into Integers;</li>
+	 *     <li>sort the Integers in descending order.</li>
+	 * </ol>
+	 * @return
+	 */
 	public List<Integer> getSortedDescendingIntegers() {
 		return new LinkedList<>();
 	}
